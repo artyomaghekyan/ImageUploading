dd77150f5da9ede1845fbab556e9d28409f9f207
artyom <artyom.aghekyan2006@gmail.com>
2023-10-29 23:43:37 +0400

    Add ViewModel

---
+++ ImageUploadingWeb/ViewModel/PersonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImageUploadingWeb.ViewModel
+{
+    public class PersonViewModel
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
