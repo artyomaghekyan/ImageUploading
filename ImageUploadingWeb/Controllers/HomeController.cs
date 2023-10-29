69461d2a23bd7f65290d9763aff69008e5f358ea
artyom <artyom.aghekyan2006@gmail.com>
2023-10-29 23:43:09 +0400

    Add Controllers

--- ImageUploadingWeb/Controllers/HomeController.cs
+++ ImageUploadingWeb/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ImageUploadingWeb.Models;
+﻿using ImageUploadingWeb.Data;
+using ImageUploadingWeb.Models;
+using ImageUploadingWeb.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,87 @@ namespace ImageUploadingWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PersonDbcontext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, PersonDbcontext context, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
+            var people = _context.People.ToList();
+            return View(people);
+        }
+
+        public IActionResult Create()
+        {
+
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(PersonViewModel vm)
+        {
+            string stringFileName = UploadFile(vm);
+            var person = new Person
+            {
+                FirstName = vm.FirstName,
+                LastName = vm.LastName,
+                Email = vm.Email,
+                Image = stringFileName
+            };
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+        public string UploadFile(PersonViewModel vm)
+        {
+            string fileName = null;
+            if (vm.Image != null)
+            {
+                string uploadDir = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+                fileName = Guid.NewGuid().ToString() + "-" + vm.Image.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    vm.Image.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || _context.People == null)
+            {
+                return NotFound();
+            }
+
+            var person = _context.People.FirstOrDefault(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var person = _context.People.FirstOrDefault(x => x.Id == id);
+            if (person != null)
+            {
+                _context.People.Remove(person);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
