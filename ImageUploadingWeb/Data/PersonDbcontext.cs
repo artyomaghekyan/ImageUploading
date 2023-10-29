52ecddc7ab7d63aff1cf4e4368a0967797118cab
artyom <artyom.aghekyan2006@gmail.com>
2023-10-29 20:51:09 +0400

    Add project files.

---
+++ ImageUploadingWeb/Data/PersonDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using ImageUploadingWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageUploadingWeb.Data
+{
+    public class PersonDbcontext : DbContext
+    {
+        public PersonDbcontext(DbContextOptions<PersonDbcontext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ ImageUploadingWeb/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImageUploadingWeb.Models
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set;}
+        public required string Email { get; set; }
+        public string Image { get; set; } = string.Empty;
+
+    }
+}
