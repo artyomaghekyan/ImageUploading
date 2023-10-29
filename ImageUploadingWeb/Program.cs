bc6d958a5aebb81fc238e581aa640532aef00b06
artyom <artyom.aghekyan2006@gmail.com>
2023-10-29 23:44:19 +0400

    Configurations

--- ImageUploadingWeb/Program.cs
+++ ImageUploadingWeb/Program.cs
@@ -1,7 +1,12 @@
+using ImageUploadingWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PersonDbcontext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
